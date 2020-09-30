a8c98de2380879d9a65e757e91ed43f3c44a1f05
Stepan <stepa.davydov.2002@mail.ru>
2020-09-30 21:36:21 +0300

    Updates

---
+++ precache-manifest.c1ce63f555cc025f8ebbc0f0e05bd9f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc36fa63b0c7f31d56dbb82b0c0dfbc7",
+    "url": "/miniapp/index.html"
+  },
+  {
+    "revision": "e38ab1000112076c87fc",
+    "url": "/miniapp/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "d676568e99ea289569fa",
+    "url": "/miniapp/static/css/main.77c7e437.chunk.css"
+  },
+  {
+    "revision": "e38ab1000112076c87fc",
+    "url": "/miniapp/static/js/2.4961477d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/miniapp/static/js/2.4961477d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d676568e99ea289569fa",
+    "url": "/miniapp/static/js/main.97e3637f.chunk.js"
+  },
+  {
+    "revision": "266a6dafcdb58601b33b",
+    "url": "/miniapp/static/js/runtime-main.17a2b5ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/miniapp/precache-manifest.0fea62643dd3373d7950f98a745999c8.js"
+  "/miniapp/precache-manifest.c1ce63f555cc025f8ebbc0f0e05bd9f0.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/miniapp/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/miniapp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
